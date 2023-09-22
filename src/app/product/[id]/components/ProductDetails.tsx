54769c77ac716f301fd02692eb3df5021423b8ec
Arystan <115387921+Ospann@users.noreply.github.com>
2023-09-22 19:02:22 +0600

    Add files via upload

--- src/app/product/[id]/components/ProductDetails.tsx
+++ src/app/product/[id]/components/ProductDetails.tsx
@@ -3,21 +3,12 @@ import { Info, Discount } from "@/components/SalesInfo";
 import { IProduct } from '@/store/product.store';
 import classes from '../product.module.css'
 import useUserStore from '@/store/user.store';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const ProductDetails = ({ name, id, rating, sold, oldPrice, description, price }: IProduct) => {
 
     const { cart, setCart } = useUserStore();
     const existInCart = cart.some(p => p.id === id);
-    const router = useRouter();
-
-    const handleAddToCart = () => {
-        if (existInCart) {
-            router.push('/cart');
-        } else {
-            setCart(id, 1);
-        }
-    };
 
     return (
         <Box className={classes.productDetails}>
@@ -29,13 +20,14 @@ const ProductDetails = ({ name, id, rating, sold, oldPrice, description, price }
             <Typography variant="body2">{description}</Typography>
             <Box className={classes.PriceBlock}>
                 <Discount oldPrice={oldPrice} price={price} />
-                <Button
-                    onClick={handleAddToCart}
-                    color='warning'
-                    variant='outlined'
-                >
-                    {existInCart ? `In Cart` : `Add to cart`}
-                </Button>
+                <Link href={'/cart'} onClick={() => { !existInCart && setCart(id, 1) }}>
+                    <Button
+                        color='warning'
+                        variant='outlined'
+                    >
+                        Buy Now
+                    </Button>
+                </Link>
             </Box>
         </Box>
     )
--- src/app/product/[id]/components/SalesBlock.tsx
+++ src/app/product/[id]/components/SalesBlock.tsx
@@ -48,7 +48,7 @@ const SalesBlock = ({ id, price }: IProduct) => {
                     variant="contained">
                     {existInCart ? "In Cart" : "Add to Cart"}
                 </Button>
-                <Link href={`/cart`} passHref onClick={() => setCart(id, step)}>
+                <Link href={`/cart`} onClick={() => setCart(id, step)}>
                     <Button
                         fullWidth
                         variant="outlined"
